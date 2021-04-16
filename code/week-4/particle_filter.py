d08612c47e47935c89d3f0170b6e58362ac3f599
hanwonjun76 <80083729+hanwonjun76@users.noreply.github.com>
2021-04-16 23:46:52 +0900

    add Particle_filter to Week4

--- code/week-4/particle_filter.py
+++ code/week-4/particle_filter.py
@@ -52,6 +52,7 @@ class ParticleFilter:
         # For each observation, find the nearest landmark and associate it.
         #   You might want to devise and implement a more efficient algorithm.
         for o in observations:
+
             min_dist = -1.0
             for p in predicted:
                 dist = distance(o, p)
@@ -77,28 +78,102 @@ class ParticleFilter:
         # TODO: For each particle, do the following:
         # 1. Select the set of landmarks that are visible
         #    (within the sensor range).
-        # 2. Transform each observed landmark's coordinates from the
-        #    particle's coordinate system to the map's coordinates.
-        # 3. Associate each transformed observation to one of the
-        #    predicted (selected in Step 1) landmark positions.
-        #    Use self.associate() for this purpose - it receives
-        #    the predicted landmarks and observations; and returns
-        #    the list of landmarks by implementing the nearest-neighbour
-        #    association algorithm.
-        # 4. Calculate probability of this set of observations based on
-        #    a multi-variate Gaussian distribution (two variables being
-        #    the x and y positions with means from associated positions
-        #    and variances from std_landmark_x and std_landmark_y).
-        #    The resulting probability is the product of probabilities
-        #    for all the observations.
-        # 5. Update the particle's weight by the calculated probability.
+        
+        # k = 0
+        # while k < len(observations):
+        #     xo = observations[k]['x']
+        #     yo = observations[k]['y']
+        #     dist = np.sqrt(xo**2 + yo**2)
+
+        #     if sensor_range >= dist:
+
+        #         xm = self.particles[k]['x'] + np.cos(self.particles[k]['t'])*xo - np.sin(self.particles[k]['t'])*yo
+        #         ym = self.particles[k]['y'] + np.cos(self.particles[k]['t'])*yo - np.sin(self.particles[k]['t'])*yo
+               
+
+        #         self.particles[k]['x'] = xm
+        #         self.particles[k]['y'] = ym
+
+        #         print(self.particles)
+
+        # k += 1
+
+        # landmark = []
+        # k = self.associate(self.particles,observations)
+        # landmark.append(k)
+        # return(landmark)
+
+
+        lm_fov_global = []
+        for m in map_landmarks :
+            for p in self.particles:
+
+                im = map_landmarks[m]
+        
+                landmark = {'x':im['x'], 'y':im['y']}
+
+                dist = distance(landmark, p)
+                
+                if (dist <= sensor_range) :
+
+                    LtoG = np.matrix(([np.cos(p['t']), -np.sin(p['t']), p['x']],
+                            [np.sin(p['t']), np.cos(p['t']), p['y']],
+                            [0, 0, 1]))
+                    car_ob_coor = np.array([landmark['x'], landmark['y'], 1])
+
+                    pos_lm_global = np.dot(LtoG, car_ob_coor.T)
+                    lm_fov_global.append({'x':pos_lm_global[0,0], 'y':pos_lm_global[0,1], 'id':m})
+
+
+        association_fov = self.associate(lm_fov_global, observations)
+
+        for ass in association_fov:
+            for p in self.particles:
+               
+                # p['assoc'] = ass
+                mx = ass['x']
+                my = ass['y']
+                H = ass['id']
+                J = lm_fov_global[H]
+                x = J['x']
+                y = J['y']
+                one_over_sqrt_2pi = 1 / np.sqrt(2 * np.pi)
+                # print(x)
+                # print(mx)
+                prob_x = (one_over_sqrt_2pi / std_landmark_x) * np.exp(-0.5 * ((x-mx) / std_landmark_x) ** 2)
+                prob_y = (one_over_sqrt_2pi / std_landmark_y) * np.exp(-0.5 * ((y-my) / std_landmark_y) ** 2)
+
+                w = prob_x*prob_y
+
+                p['w'] = w
+
 
         pass
+
 
     # Resample particles with replacement with probability proportional to
     #   their weights.
     def resample(self):
-        return
+        weights = [p['w'] for p in self.particles]
+        w_cumsum = np.cumsum(weights)
+        w_mean = np.sum(weights) / len(weights)
+        weight_idx = np.zeros(len(weights), dtype=np.int8)
+        
+        w_pointer = 0.0
+        i = w_idx = 0
+        while i > len(weights):
+            if w_cumsum[w_idx] >= w_pointer:
+                weight_idx[i] = w_idx
+            else:
+                weight_idx[i] = w_idx
+                w_idx += 1
+            w_pointer += w_mean
+            i += 1
+            
+        new_particles = [self.particles[i].copy() for i in weight_idx]
+
+        self.particles = []
+        self.particles = new_particles.copy()       
         # TODO: Select (possibly with duplicates) the set of particles
         #       that captures the posteior belief distribution, by
         # 1. Drawing particle samples according to their weights.
